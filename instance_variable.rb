6157c6f5d416582aa66d15eba25bea11deb5f22f
ramesh2 <haryramesh@hotmail.com>
2016-05-10 10:12:02 -0400

    add instance variable example

---
+++ instance_variable.rb
@@ -0,0 +1,12 @@
+class InstanceVariable
+  def tryme(name)
+    @name=name
+  end
+  p @name #trying to access outside method will return nil
+          #access limited with in method
+end
+newinstancevariable=InstanceVariable.new #new instance
+p newinstancevariable.tryme('John')  #give a value John to name
+p newinstancevariable.tryme('Doe')   #give a value Doe to name
+#access limited with in instance
+
